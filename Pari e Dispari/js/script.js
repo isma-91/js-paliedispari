6dbd3007f12b72785c4eb89ead4829874d9a7826
isma-91 <isma91@hotmail.it>
2022-10-28 16:00:54 +0200

    add palindromo

---
+++ Pari e Dispari/js/script.js
@@ -0,0 +1,7 @@
+/*
+prompt per chiedere paro o dispari all'utente 
+prompt per fargli scegliere un numero
+funzione per creare un numero random per il computer
+sommare i 2 numeri
+funzione per vedere se la somma dei 2 numeri è pari o dispari
+*/
